72bfc4331c58ebe5f372a3477a37b77369c9a0e8
Aleksey Dudnikov <xthelastsecretx@gmail.com>
2023-04-11 12:42:47 +0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+%w[Ruby Python Go Rust Kotlin].each { Skill.find_or_create_by!(name: _1)}
+%w[Coding Reading Sport Videogames].each { Interest.find_or_create_by!(name: _1) }
