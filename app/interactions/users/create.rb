854821f9917c4cefcd2c1d28d3f667d2d3d290a4
Aleksey Dudnikov <xthelastsecretx@gmail.com>
2023-04-11 12:43:14 +0300

    Users::Create Interaction refactoring

--- app/interactions/users/create.rb
+++ app/interactions/users/create.rb
@@ -1,16 +1,5 @@
 module Users
   class Create < ActiveInteraction::Base
-    EXAMPLE_PARAMS = {
-      name: "Aleksey",
-      surname: "Dudnikov",
-      # patronymic: "Alekseevich",
-      email: "xthelastsecretx@gmail.com",
-      age: 33,
-      nationality: "Russian",
-      country: "Russia",
-      gender: "male"
-    }
-
     hash :params do
       string :name
       string :surname
@@ -23,29 +12,25 @@ module Users
     end
 
     def execute
-      debugger
-      return if User.where(email: params[:email].downcase).exists?
+      return if User.where('LOWER(email) = LOWER(?)', params[:email]).exists?
       return unless (1..90).include?(params[:age])
       return unless %w[male female].include?(params[:gender])
 
       user_full_name = params.values_at(:surname, :name, :patronymic).join(" ").strip
 
       user_params = params.except(:interests)
-      user = User.create(user_params.merge(fullname: user_full_name))
+      User.transaction do
+        user = User.new(user_params.merge(fullname: user_full_name))
 
-      Interest.where(name: params["interests"]).each do |interest|
-          user.interests = user.interest + interest
-          user.save!
+        Interest.where(name: params[:interests]).find_each do |interest|
+          user.interests << interest
         end
 
-      user_skills = []
-      params["skills"].split(",").each do |skil|
-          skil = Skil.find(name: skil)
-          user_skills = user_skills + [skil]
+        Skill.where(name: params[:skills]&.split(',')).find_each do |skill|
+          user.skills << skill
         end
-
-      user.skills = user_skills
-      user.save
+        user.save!
+      end
     end
   end
 end
